0fd22790eac209b8992c4be8e72a88c4c9aa2acc
Hp <Hp@DESKTOP-T1P3QBK>
2026-03-24 16:57:52 +0530

    commit prog1

--- DummyE40/src/test/java/demo/Program1.java
+++ DummyE40/src/test/java/demo/Program1.java
@@ -1,5 +1,14 @@
 package demo;
-
+/* 
+ * clone:===>  click on File -> import--> Git--> Project from git--> clone url --> 
+ * give the project url from git the saved repo url and the new junior users account details un and password (token) and continue 
+ * 
+ * 
+ * fork:==> steal someones repo professionally for reference may be 
+ * search for any framework you want --> click on fork --> create fork--> give new name/keep same--> create fork -> 
+ * the forked framework will get copied into your repo
+ * 
+ */
 public class Program1 {
 
 	public static void main(String[] args) {
